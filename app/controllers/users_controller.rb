ac5d33d498db9767ad670fe210b7c961dc773fe8
vagrant <vagrant@localhost.localdomain>
2020-03-24 06:48:34 +0000

    [fix]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @user = User.find(params[:id])
-    @book = Book.new
   end
+  
   def show
   	@user = User.find(params[:id])
     @book = Book.new
+    @books = Book.all
   end
 
   def edit
